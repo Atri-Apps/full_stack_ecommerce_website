c47a688c3da983a3463102caa60184aafa62059c
sanskargupta <sanskarg348@gmail.com>
2022-09-23 12:19:48 +0530

    1st commit

--- controllers/routes/main.py
+++ controllers/routes/main.py
@@ -8,6 +8,7 @@ def init_state(at: Atri):
     The argument "at" is a dictionary that has initial values set from visual editor.
     Changing values in this dictionary will modify the intial state of the app.
     """
+
     pass
 
 def handle_page_request(at: Atri, req: Request, res: Response, query: str):
